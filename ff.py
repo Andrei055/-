2d4ca82fe0a5ba2c65619545fdd15d950c3e00fc
Andrei055 <albbg55@gmail.com>
2024-09-07 15:54:49 +0300

    Add files via upload

---
+++ ff.py
@@ -0,0 +1,19 @@
+#1st program
+print((9**0.5)*5)
+
+#2nd program
+print(9.99>9.98 and  1000 != 1000.1)
+
+#3rd program
+a = (2*2+2)
+b = (2*(2+2))
+print(2*2+2)
+print(2*(2+2))
+print(a == b)
+
+#4th program
+a = '123.456'
+print(a[4:5])
+#другой способ
+a = '123.456'
+print(int(((int(float(a)*10) /10)*10)%10))
